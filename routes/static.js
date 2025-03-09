9a7de4e6ec561bde0e11e906db61e9ddfa06e159
PlotinaArhin <arhinplotina@gmail.com>
2025-03-09 22:26:58 +0000

    made change

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,24 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+const app = express();
 
-// Static Routes
-// Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+// View engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
-module.exports = router;
+// Serve static files (CSS, images)
+app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" });
+});
 
-
+// Start server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
